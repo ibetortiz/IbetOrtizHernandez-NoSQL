b42e2ec9fc43c4f55b205d30489f7545e5a86f0e
ibetortiz <ortizibet4@gmail.com>
2020-07-20 21:22:05 -0500

    React

---
+++ AprendiendoReact/src/Components/MyComponent.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+ 
+class MyComponent extends Component{
+
+    render(){
+     let receta = {
+         name : 'Pizza',
+         ingredientes:['Tomate','Queso','Jamon cocido'],
+         calorias : 400
+     };
+
+
+        return (
+            <React.Fragment>
+            <h1>{'Receta:'+ receta.name}</h1>
+        <h2>{'Calorias:'+ receta.calorias}</h2>
+        <ol>
+            {
+                receta.ingredientes.map((ingrediente,i) => {
+                console.log(ingrediente);
+                return (
+                    <li  key={i}>
+                        {ingrediente}
+                    </li>
+                );
+                })
+            }
+        </ol>
+            </React.Fragment>
+        );
+    }
+
+}
+
+export default MyComponent;
